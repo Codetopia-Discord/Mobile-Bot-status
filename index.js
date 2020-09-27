7c10ee23a8f9b2940ceefe50e27b0c48dafd1390
DiveshTheReal <60512800+DiveshTheReal@users.noreply.github.com>
2020-09-27 18:10:30 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+const Constants = require ("discord.js/src/util/Constants.js")
+Constants.DefaultOptions.ws.properties.$browser= "Discord iOS"
+
+//Add This to your index.js or server.js what ever your main and it will works :D 
